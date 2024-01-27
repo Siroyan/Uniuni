8b2c2dbf6cf5dc5a4bc8551ab61b1287b5601845
Siroyan <1517053@ed.tus.ac.jp>
2024-01-27 21:13:14 +0900

    メニューとバージョンダイアログを追加

--- Uniuni/main.cpp
+++ Uniuni/main.cpp
@@ -1,7 +1,10 @@
 #define STRICT
 #include <windows.h>
 
-LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
+#include "resource.h"
+
+LRESULT	CALLBACK	WindowProc			(HWND, UINT, WPARAM, LPARAM);
+BOOL	CALLBACK	DialogWindowProc	(HWND, UINT, WPARAM, LPARAM);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevious, LPSTR lpszCmdLine, int nCmdShow)
 {
@@ -13,21 +16,22 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevious, LPSTR lpszCmdLine, 
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
-	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_UNIUNI));
 	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-	wc.lpszMenuName = NULL;
+	wc.lpszMenuName = MAKEINTRESOURCE(IDR_MENU1);
 	wc.lpszClassName = "WindowClassName";
+
 	RegisterClass(&wc);
 	hWnd = CreateWindow
 	(
 		"WindowClassName",
-		"Title Bar String",
-		WS_POPUP | WS_CAPTION | WS_THICKFRAME | WS_SYSMENU | WS_MAXIMIZEBOX,
+		"Uniuni",
+		WS_POPUP | WS_CAPTION | WS_SYSMENU | WS_MAXIMIZEBOX,
 		100, 100,
 		1500, 900,
 		(HWND)NULL,
-		(HMENU)NULL,
+		NULL,
 		hInstance,
 		NULL
 	);
@@ -40,7 +44,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevious, LPSTR lpszCmdLine, 
 	}
 	return (int)msg.wParam;
 }
-LRESULT CALLBACK WindowProc(HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lParam)
+
+LRESULT	CALLBACK WindowProc(HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (nMsg)
 	{
@@ -50,6 +55,55 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lParam)
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case ID_HELP_VERSION:
+
+			DialogBox(
+				(HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+				MAKEINTRESOURCE(IDD_DIALOG_VERSION),
+				hWnd,
+				(DLGPROC)DialogWindowProc);
+			break;
+		case ID_FILE_QUIT:
+			DestroyWindow(hWnd);
+			return 0;
+		default:
+			break;
+		}
 	}
 	return DefWindowProc(hWnd, nMsg, wParam, lParam);
+}
+
+BOOL CALLBACK DialogWindowProc(HWND hWnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	static	RECT	rt, rtParent;
+	LONG	w, h, x, y;
+	switch (iMsg)
+	{
+	case WM_INITDIALOG:
+		//モーダルダイアログボックスを作成
+		GetWindowRect(hWnd, &rt);
+		GetWindowRect(GetParent(hWnd), &rtParent);
+		w = rt.right - rt.left;
+		h = rt.bottom - rt.top;
+		x = rtParent.right / 2 - w / 2;
+		y = rtParent.bottom / 2 - h / 2;
+		SetWindowPos(hWnd, 0, x, y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+		return TRUE;
+	case WM_CLOSE:
+		EndDialog(hWnd, 0);
+		return FALSE;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case ID_DIALOG_VERSION_CLOSE:
+			EndDialog(hWnd, 0);
+			break;
+		}
+		return TRUE;
+	default:
+		return FALSE;
+	}
 }
